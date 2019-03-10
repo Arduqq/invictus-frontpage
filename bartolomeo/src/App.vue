91dbcd9945637cfc6d0520bc022d6e56aa60a60a
arduqq <twop@hotmail.de>
2019-03-10 01:08:13 +0100

    Fixed wrong vue

--- bartolomeo/src/App.vue
+++ bartolomeo/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Quotes/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quotes from './components/quotes.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Quotes
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
